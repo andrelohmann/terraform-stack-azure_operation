6043f26ca5eba46de5f893bccb7ab11ceacc91c7
Andre Lohmann <lohmann.andre@gmail.com>
2019-05-28 18:44:57 +0200

    first problems fixed

--- SecurityGroups.tf
+++ SecurityGroups.tf
@@ -3,7 +3,7 @@ resource "azurerm_network_security_group" "vpn_gateway" {
     location            = var.location
     resource_group_name = azurerm_resource_group.operation_stack.name
 
-    security_rule = {
+    security_rule {
         name                       = "SSH"
         priority                   = 1001
         direction                  = "Inbound"
@@ -23,6 +23,76 @@ resource "azurerm_network_security_group" "vpn_gateway" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "1194"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment                   = var.environment
+        stack_name                    = var.stack_name
+    }
+}
+
+resource "azurerm_network_security_group" "jenkins" {
+    name                = "${var.stack_name}-jenkins-sg"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.operation_stack.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "http"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment                   = var.environment
+        stack_name                    = var.stack_name
+    }
+}
+
+resource "azurerm_network_security_group" "gitlab" {
+    name                = "${var.stack_name}-gitlab-sg"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.operation_stack.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "http"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 module "vpn-gateway" {
-  source                                      = "https://github.com/andrelohmann/terraform-module-azure_vm.git"
+  source                                      = "github.com/andrelohmann/terraform-module-azure_vm"
   location                                    = var.location
   environment                                 = var.environment
-  stack_name                                  = "${var.stack_name}-gateway"
+  stack_name                                  = "${var.stack_name}"
+  vm_name                                     = "${var.stack_name}-gateway"
   resource_group_name                         = azurerm_resource_group.operation_stack.name
   computer_name                               = "${var.stack_name}-gateway"
   vm_size                                     = "Standard_B1ls"
@@ -12,6 +13,45 @@ module "vpn-gateway" {
   boot_diagnostics_uri                        = azurerm_storage_account.operation_stack.primary_blob_endpoint
   network_security_group_id                   = azurerm_network_security_group.vpn_gateway.id
   subnet_id                                   = azurerm_subnet.subnet01.id
-  private_ip_addres                           = "10.10.1.10"
+  private_ip_address_allocation               = "Static"
+  private_ip_address                          = "10.10.1.10"
   public_ip_address_id                        = azurerm_public_ip.vpn_gateway.id
+}
+
+module "jenkins" {
+  source                                      = "github.com/andrelohmann/terraform-module-azure_vm"
+  location                                    = var.location
+  environment                                 = var.environment
+  stack_name                                  = "${var.stack_name}"
+  vm_name                                     = "${var.stack_name}-jenkins"
+  resource_group_name                         = azurerm_resource_group.operation_stack.name
+  computer_name                               = "${var.stack_name}-jenkins"
+  vm_size                                     = "Standard_B2s"
+  vm_tags                                     = { ephemeral = false }
+  admin_user                                  = var.admin_user
+  pub_key                                     = var.pub_key
+  boot_diagnostics_uri                        = azurerm_storage_account.operation_stack.primary_blob_endpoint
+  network_security_group_id                   = azurerm_network_security_group.jenkins.id
+  subnet_id                                   = azurerm_subnet.subnet02.id
+  private_ip_address_allocation               = "Static"
+  private_ip_address                          = "10.10.2.10"
+}
+
+module "gitlab" {
+  source                                      = "github.com/andrelohmann/terraform-module-azure_vm"
+  location                                    = var.location
+  environment                                 = var.environment
+  stack_name                                  = "${var.stack_name}"
+  vm_name                                     = "${var.stack_name}-gitlab"
+  resource_group_name                         = azurerm_resource_group.operation_stack.name
+  computer_name                               = "${var.stack_name}-gitlab"
+  vm_size                                     = "Standard_B2s"
+  vm_tags                                     = { ephemeral = false }
+  admin_user                                  = var.admin_user
+  pub_key                                     = var.pub_key
+  boot_diagnostics_uri                        = azurerm_storage_account.operation_stack.primary_blob_endpoint
+  network_security_group_id                   = azurerm_network_security_group.gitlab.id
+  subnet_id                                   = azurerm_subnet.subnet03.id
+  private_ip_address_allocation               = "Static"
+  private_ip_address                          = "10.10.3.10"
 }
